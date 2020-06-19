bb608de05ef4ca15e6d4339556764e36b0d1d49f
Grayson Deese <gldeese@ncsu.edu>
2020-06-19 19:12:07 -0400

    brick skeleton

--- scirpt.js
+++ scirpt.js
@@ -2,9 +2,13 @@
 const rulesBtn = document.getElementById("rules-btn");
 const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
+// canvas vars
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
 // Event handlers to show rules
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
@@ -49,6 +53,25 @@ function drawPaddle() {
 function drawScore(){
     ctx.font = '20px Arial';
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+// Brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+// Create bricks
+const bricks = [];
+for(let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
 }
 // Drawing everything
 function draw() {
