6ab28b6e9fcd00ee200613a3a87c2f2633343b0a
Grayson Deese <gldeese@ncsu.edu>
2020-06-19 19:00:46 -0400

    paddle and score drawn.

--- scirpt.js
+++ scirpt.js
@@ -1,30 +1,59 @@
 // Setting all vars
-const rulesBtn = document.getElementById('rules-btn');
-const closeBtn = document.getElementById('close-btn');
-const rules = document.getElementById('rules');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let score = 0;
+// Event handlers to show rules
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
+
 // Ball properties
 const ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 10,
-    speed: 4,
-    dx: 4,
-    dy: -4
-}
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
 // Drawing ball
 function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Paddle properties
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+// Drawing paddle
+function drawPaddle() {
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-    ctx.fillStyle = '#0095dd'
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = "#0095dd";
     ctx.fill();
     ctx.closePath();
 }
 
-drawBall();
-// Event handlers to show rules
-rulesBtn.addEventListener('click', () => 
-    rules.classList.add('show'));
-closeBtn.addEventListener('click', () => 
-    rules.classList.remove('show'));
+// Drawing score
+function drawScore(){
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+// Drawing everything
+function draw() {
+    drawBall();
+    drawPaddle();
+    drawScore();
+}
+draw();
